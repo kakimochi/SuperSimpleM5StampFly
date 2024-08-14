61551a33d0566569b115bba23179aee37481ee90
kakimochi <konydrive@hotmail.com>
2024-08-14 23:30:37 +0900

    はじめの六歩: ブザーを鳴らしてみよう！！

--- JoyStick/src/main.cpp
+++ JoyStick/src/main.cpp
@@ -1,22 +1,49 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+// LED
 #define NUM_LEDS_JOYSTICK 2
-#define PIN_LED_JOYSTICK 6  // G6 on the M5AtomS3 Joystick
+#define PIN_LED_JOYSTICK 6 // G6 on the M5AtomS3 Joystick
 CRGB leds_joystick[NUM_LEDS_JOYSTICK];
+
+// Beep Sound
+#define PIN_BUZZER 5       // G5 on the M5AtomS3 Joystick
+#define TONE_C5 523 // 523.251
+#define TONE_E5 659 // 659.255
+#define TONE_G5 784 // 783.991
+#define TONE_C6 (TONE_C5 * 2)
+#define TONE_E6 (TONE_E5 * 2)
+#define TONE_G6 (TONE_G5 * 2)
+
+void beep()
+{
+    tone(PIN_BUZZER, TONE_E5, 200);
+}
+
+void beep_init_done()
+{
+    tone(PIN_BUZZER, TONE_C5, 500);
+    tone(PIN_BUZZER, TONE_E5, 500);
+    tone(PIN_BUZZER, TONE_G5, 500);
+}
 
 void setup()
 {
     USBSerial.begin(115200);
 
     FastLED.addLeds<WS2812, PIN_LED_JOYSTICK, GRB>(leds_joystick, NUM_LEDS_JOYSTICK);
+    FastLED.setBrightness(50);
 
+    pinMode(PIN_BUZZER, OUTPUT);
+
+    beep_init_done();
     USBSerial.println("[info] init done.");
 }
 
 void loop()
 {
     USBSerial.println("[info] Hello JoyStick!");
+    beep();
 
     auto colors = {
         CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::Yellow, CRGB::Purple,
@@ -25,8 +52,7 @@ void loop()
         CRGB::Black, CRGB::Gray, CRGB::Brown, CRGB::Maroon, CRGB::Navy,
         CRGB::Olive, CRGB::SkyBlue, CRGB::SlateGray, CRGB::DarkGreen,
         CRGB::DarkOrange, CRGB::DarkViolet, CRGB::DarkRed, CRGB::DarkBlue,
-        CRGB::DarkCyan, CRGB::DarkMagenta
-    };
+        CRGB::DarkCyan, CRGB::DarkMagenta};
 
     for (auto color : colors)
     {
