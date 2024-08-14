6b13350f0496f217d9327c291f3847c6b176b3dd
kakimochi <konydrive@hotmail.com>
2024-08-14 10:51:09 +0900

    はじめの三歩: Lチカしてみる

--- StampFly/src/main.cpp
+++ StampFly/src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define NUM_LEDS 1
+#define PIN_LED 21  // G21 on the M5StampS3
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+    USBSerial.begin(115200);
+
+    FastLED.addLeds<WS2812, PIN_LED, GRB>(leds, NUM_LEDS);
+    USBSerial.println("[info] init done.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    USBSerial.println("[info] Hello World!");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    auto colors = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::White, CRGB::Black};
+
+    for(auto color : colors)
+    {
+        leds[0] = color;
+        FastLED.show();
+        delay(1000);
+    }
+
 }
