d8e02c2908d7f1bf694bc858fecb9d279052540b
kakimochi <konydrive@hotmail.com>
2024-08-15 14:53:25 +0900

    buzzerもM5Unified化 (issue:音われる)

--- JoyStick/src/main.cpp
+++ JoyStick/src/main.cpp
@@ -18,9 +18,9 @@ CRGB leds_joystick[NUM_LEDS_JOYSTICK];
 
 // Beep Sound
 #define PIN_BUZZER 5       // G5 on the M5AtomS3 Joystick
-#define TONE_C5 523 // 523.251
-#define TONE_E5 659 // 659.255
-#define TONE_G5 784 // 783.991
+#define TONE_C5 523.251
+#define TONE_E5 659.255
+#define TONE_G5 783.991
 #define TONE_C6 (TONE_C5 * 2)
 #define TONE_E6 (TONE_E5 * 2)
 #define TONE_G6 (TONE_G5 * 2)
@@ -32,14 +32,15 @@ unsigned long current_ms = 0;
 
 void beep()
 {
-    tone(PIN_BUZZER, TONE_E5, 200);
+    M5.Speaker.tone(TONE_E5, 200);
 }
 
 void beep_init_done()
 {
-    tone(PIN_BUZZER, TONE_C5, 500);
-    tone(PIN_BUZZER, TONE_E5, 500);
-    tone(PIN_BUZZER, TONE_G5, 500);
+    // tone(PIN_BUZZER, TONE_C5, 500);     // 音きれい
+    M5.Speaker.tone(TONE_C5, 500);   // 音スカる、なんでだろうね
+    // M5.Speaker.tone(TONE_E5, 500);
+    // M5.Speaker.tone(TONE_G5, 500);
 }
 
 void led_color_update()
@@ -65,8 +66,10 @@ void setup()
     M5.begin(cfg);
     USBSerial.begin(115200);
 
-    // hardware config
-    pinMode(PIN_BUZZER, OUTPUT);
+    // buzzer
+    auto spk_cfg = M5.Speaker.config();
+    spk_cfg.pin_data_out = PIN_BUZZER;
+    M5.Speaker.config(spk_cfg);
 
     // LED
     FastLED.addLeds<WS2812, PIN_LED_JOYSTICK, GRB>(leds_joystick, NUM_LEDS_JOYSTICK);
