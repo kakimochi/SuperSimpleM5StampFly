62baa351e30f8d55a25532b370cd2de0aebe2cf7
kakimochi <konydrive@hotmail.com>
2024-08-16 16:48:22 +0900

    はじめの十二歩: ジョイスティックの位置を画面に表示する

--- JoyStick/include/driver_joystick.h
+++ JoyStick/include/driver_joystick.h
@@ -32,12 +32,23 @@ namespace JOY
         {
             uint16_t x; // 12bit
             uint16_t y;
-        } joy1;
+        } stick_l_raw;
         struct
         {
             uint16_t x; // 12bit
             uint16_t y;
-        } joy2;
+        } stick_r_raw;
+        struct
+        {
+            float_t x;
+            float_t y;
+        } stick_l;
+        struct
+        {
+            float_t x;
+            float_t y;
+        } stick_r;
+
         uint8_t btn[NUM_BTN_MAX];
     } JoyData_t;
 
@@ -50,22 +61,40 @@ namespace JOY
 
     void update()
     {
-        I2C::read(I2C_ADDR_ATOM_JOYSTICK, I2C_REG_JOY1_ADC_VALUE_12BITS, (uint8_t *)&joy_data.joy1, sizeof(joy_data.joy1));
-        I2C::read(I2C_ADDR_ATOM_JOYSTICK, I2C_REG_JOY2_ADC_VALUE_12BITS, (uint8_t *)&joy_data.joy2, sizeof(joy_data.joy2));
+        I2C::read(I2C_ADDR_ATOM_JOYSTICK, I2C_REG_JOY1_ADC_VALUE_12BITS, (uint8_t *)&joy_data.stick_l_raw, sizeof(joy_data.stick_l_raw));
+        I2C::read(I2C_ADDR_ATOM_JOYSTICK, I2C_REG_JOY2_ADC_VALUE_12BITS, (uint8_t *)&joy_data.stick_r_raw, sizeof(joy_data.stick_r_raw));
         I2C::read(I2C_ADDR_ATOM_JOYSTICK, I2C_BTN_REG, joy_data.btn, sizeof(joy_data.btn));
+
+        // normalize
+        joy_data.stick_l.x = (float)(joy_data.stick_l_raw.x - 2048) / 2048.0;
+        joy_data.stick_l.y = (float)(joy_data.stick_l_raw.y - 2048) / 2048.0;
+        joy_data.stick_r.x = (float)(joy_data.stick_r_raw.x - 2048) / 2048.0;
+        joy_data.stick_r.y = (float)(joy_data.stick_r_raw.y - 2048) / 2048.0;
+
+        // correct axis
+        joy_data.stick_l.y *= -1.0;
+        joy_data.stick_r.y *= -1.0;
     }
 
-    void getAxis(uint8_t index, uint16_t *x, uint16_t *y)
+    void getAxis(uint8_t index, uint16_t *x, uint16_t *y, bool normalized = false)
     {
         if (index == STICK_L)
         {
-            *x = joy_data.joy1.x;
-            *y = joy_data.joy1.y;
-        }
-        else if (index == STICK_R)
-        {
-            *x = joy_data.joy2.x;
-            *y = joy_data.joy2.y;
+            if(normalized) {
+                *x = joy_data.stick_l.x;
+                *y = joy_data.stick_l.y;
+            } else {
+                *x = joy_data.stick_l_raw.x;
+                *y = joy_data.stick_l_raw.y;
+            }
+        } else if (index == STICK_R) {
+            if(normalized) {
+                *x = joy_data.stick_r.x;
+                *y = joy_data.stick_r.y;
+            } else {
+                *x = joy_data.stick_r_raw.x;
+                *y = joy_data.stick_r_raw.y;
+            }
         }
     }
 
--- JoyStick/src/main.cpp
+++ JoyStick/src/main.cpp
@@ -27,7 +27,7 @@ void setup()
     M5.Display.setFont(&fonts::efontCN_10);
     M5.Display.setTextColor(GOLD);
     M5.Display.setTextSize(2);
-    M5.Display.drawString("JoyStick!", 0, M5.Display.height() / 2);
+    M5.Display.drawString("JoyCon!", M5.Display.width()-100, 0);
     M5.Display.endWrite();
 
     // Application timer
@@ -46,14 +46,17 @@ void loop()
         static bool toggle_switch;
         if (toggle_switch)
         {
-            M5.Display.fillScreen(BLACK);
+            M5.Display.fillScreen(WHITE);
         }
         else
         {
-            M5.Display.fillScreen(GOLD);
+            M5.Display.fillScreen(BLACK);
         }
         toggle_switch = !toggle_switch;
     }
+
+    M5.Display.fillCircle(JOY::joy_data.stick_l_raw.x / 32, JOY::joy_data.stick_l_raw.y / 32, 5, GOLD);
+    M5.Display.fillCircle(JOY::joy_data.stick_r_raw.x / 32, JOY::joy_data.stick_r_raw.y / 32, 5, CYAN);
 
     // Application timer
     current_ms = millis();
@@ -61,8 +64,8 @@ void loop()
     {
         LED::color_rotation();
         LED::update();
-        USBSerial.printf("[info] button_state : %1x%1x%1x%1x\n", JOY::isPressed(JOY::BTN_TRG_L), JOY::isPressed(JOY::BTN_TRG_R), JOY::isPressed(JOY::BTN_STICK_L), JOY::isPressed(JOY::BTN_STICK_R));
-
+        // USBSerial.printf("[info] button_state : %1x%1x%1x%1x\n", JOY::isPressed(JOY::BTN_TRG_L), JOY::isPressed(JOY::BTN_TRG_R), JOY::isPressed(JOY::BTN_STICK_L), JOY::isPressed(JOY::BTN_STICK_R));
+        USBSerial.printf("[info] stick_l: %.3f, %.3f, stick_r: %.3f, %.3f\n", JOY::joy_data.stick_l.x, JOY::joy_data.stick_l.y, JOY::joy_data.stick_r.x, JOY::joy_data.stick_r.y);
         pre_ms = current_ms;
     }
 
